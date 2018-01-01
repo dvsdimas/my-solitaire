36091dcdfabe4afd12b4f8389411831d3bb949db
Dmitry Mylnev <Dmitry.Mylnev@Markets.com>
2018-01-01 21:40:44 +0300

    added core functions for card transfer

--- src/my_solitaire/core.clj
+++ src/my_solitaire/core.clj
@@ -1,6 +1,42 @@
 (ns my-solitaire.core
   (:gen-class))
 
+
+;; card
+;; (card/make :diamonds :2)
+
+;; stack
+;; {:down (list card) :up (list card)}
+
+;; game
+;; {:foundations [stack x4]
+;;  :stock       [stack x1]
+;;  :tableau     [stack x7]}
+
+;; move
+;; (fn [game] ... new-game)
+
+
+(defn- move-fn [count from to transform]
+  (fn [game]
+    (let [cards (take count (get-in game from))]
+      (-> game
+          (update-in from (partial drop count))
+          (update-in to into (transform cards))))))
+
+(defn flip-fn [count from to]
+  (move-fn count from to identity))
+
+(defn transfer-fn [count from to]
+  (move-fn count from to reverse))
+
+
+
+
+
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/my_solitaire/core_test.clj
+++ test/my_solitaire/core_test.clj
@@ -3,4 +3,92 @@
             [my-solitaire.core :refer :all]
             [my-solitaire.card :as card]))
 
+(deftest tableau0-tableau1
+  (let [game {:tableau [{:up (list
+                               (card/make :diamonds :3)
+                               (card/make :clubs :4)
+                               (card/make :hearts :5))}
+                        {:up (list
+                               (card/make :spades :6))}]}
+        game2 {:tableau [{:up (list)}
+                         {:up (list
+                                (card/make :diamonds :3)
+                                (card/make :clubs :4)
+                                (card/make :hearts :5)
+                                (card/make :spades :6))}]}
+
+        move (transfer-fn 3 [:tableau 0 :up] [:tableau 1 :up])]
+    (is (= game2 (move game)))))
+
+(deftest tableau0-foundation0
+  (let [game {:foundations [{:up (list
+                                   (card/make :diamonds :2)
+                                   (card/make :diamonds :A))}]
+              :tableau [{:up (list
+                               (card/make :diamonds :3)
+                               (card/make :clubs :4)
+                               (card/make :hearts :5))}]}
+        game2 {:foundations [{:up (list
+                                    (card/make :diamonds :3)
+                                    (card/make :diamonds :2)
+                                    (card/make :diamonds :A))}]
+               :tableau [{:up (list
+                                (card/make :clubs :4)
+                                (card/make :hearts :5))}]}
+
+        move (transfer-fn 1 [:tableau 0 :up] [:foundations 0 :up])]
+
+    (is (= game2 (move game)))))
+
+(deftest stock-down-stock-up
+  (let [game {:stock [{:down (list
+                               (card/make :spades :A)
+                               (card/make :diamonds :K)
+                               (card/make :diamonds :Q)
+                               (card/make :clubs :6))
+                       :up (list
+                             (card/make :diamonds :2)
+                             (card/make :diamonds :A))}]}
+        game2 {:stock [{:down (list
+                                (card/make :clubs :6))
+                        :up (list
+                              (card/make :diamonds :Q)
+                              (card/make :diamonds :K)
+                              (card/make :spades :A)
+                              (card/make :diamonds :2)
+                              (card/make :diamonds :A))}]}
+
+        move (flip-fn 3 [:stock 0 :down] [:stock 0 :up])]
+
+    (is (= game2 (move game)))))
+
+(deftest tableau-down-tableau-up
+  (let [game {:tableau [{:down (list
+                                 (card/make :spades :A)
+                                 (card/make :diamonds :K)
+                                 (card/make :diamonds :Q)
+                                 (card/make :clubs :6))
+                         :up (list)}]}
+        game2 {:tableau [{:down (list
+                                  (card/make :diamonds :K)
+                                  (card/make :diamonds :Q)
+                                  (card/make :clubs :6))
+                          :up (list
+                                (card/make :spades :A))}]}
+
+        move (flip-fn 1 [:tableau 0 :down] [:tableau 0 :up])]
+
+    (is (= game2 (move game)))))
+
+
+
+
+
+
+
+
+
+
+
+
 
